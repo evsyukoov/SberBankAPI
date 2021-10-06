90d87dcd99dc56b948280ff1c4f95e00755a8e98
evsyukoov <den16121993>
2021-10-06 18:01:05 +0300

    fix init sql

--- src/main/java/com/evsyukoov/project/H2Server.java
+++ src/main/java/com/evsyukoov/project/H2Server.java
@@ -1,10 +1,11 @@
 package com.evsyukoov.project;
 
 import com.evsyukoov.project.errors.ServerError;
-import com.evsyukoov.project.init.DataBaseInitializerParser;
 import com.evsyukoov.project.messages.Message;
+import org.h2.tools.RunScript;
 import org.h2.tools.Server;
 
+import java.io.FileReader;
 import java.sql.*;
 
 public class H2Server {
@@ -18,10 +19,20 @@ public class H2Server {
             e.printStackTrace();
             throw new ServerError(Message.H2_ERROR);
         }
-        new DataBaseInitializerParser().load();
+        initDb();
     }
 
     public static void stop() throws Exception {
         server.stop();
+    }
+
+    private static void initDb() throws Exception {
+        Connection connection = DriverManager.getConnection("jdbc:h2:~/BankTask", "sa","");
+        RunScript.execute(connection, new FileReader(
+                H2Server.class.getClassLoader().getResource("init/drop_schema.sql").getFile()));
+        RunScript.execute(connection, new FileReader(
+                H2Server.class.getClassLoader().getResource("init/init_schema.sql").getFile()));
+        RunScript.execute(connection, new FileReader(
+                H2Server.class.getClassLoader().getResource("init/data.sql").getFile()));
     }
 }
--- src/main/java/com/evsyukoov/project/SpringServer.java
+++ src/main/java/com/evsyukoov/project/SpringServer.java
@@ -6,14 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringServer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         try {
             H2Server.run();
+            SpringApplication.run(SpringServer.class, args);
         } catch (Exception e) {
             System.out.println(e.getMessage());
-            return;
         }
-
-        SpringApplication.run(SpringServer.class, args);
     }
 }
